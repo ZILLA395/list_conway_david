e8744c0ae68a3da0b2178b0e35ba73dd203a703d
ZILLA395 <daconway14@gmail.com>
2024-09-19 14:36:36 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿namespace list_conway_david
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> games = new List<string> 
+            { 
+               "Cs:GO",
+               "Halo 4",
+               "ARK",
+               "minecraft",
+               "endenring"
+
+            };
+
+            string[] othergames = new string[]
+            {
+              "farcry primal",
+              "Overwatch 2",
+            };
+
+            foreach(string game in games)
+            { 
+             Console.WriteLine(game);
+            }
+
+
+            Console.WriteLine($"Games in list: {games.Count}");
+
+            games.AddRange(othergames);
+
+            Console.WriteLine($"Games in list: {games.Count}");
+
+            if (games.Contains("Halo"))
+            {
+                Console.WriteLine("masterchief is in the house!");
+            }
+            else
+            {
+                games.Add("Halo");
+            }
+
+            int myInt = 6;
+
+            if (myInt < games.Count) 
+            {
+                games.RemoveAt(myInt);
+            }
+            else
+            {
+                Console.WriteLine("game not found");
+            }
+
+            Console.WriteLine("all games in list:");
+            foreach(string game in games)
+            { 
+                Console.WriteLine(game); 
+            }
+            games.Sort();
+
+            Console.WriteLine("sorted games list:");
+            foreach(string game in games)
+            {
+                Console.WriteLine(game);
+            }
+
+            string[] newList = new string[games.Count];
+
+            games.CopyTo(newList);
+
+            games.Clear();
+
+            Console.WriteLine("new list:");
+            foreach(string game in newList)
+            {
+                Console.WriteLine(game);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# list_conway_david
+ 
